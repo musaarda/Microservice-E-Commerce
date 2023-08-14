ba79f88264d89a418069677e9b932b65114ebb58
Musa Arda <musa.arda@twiser.com>
2023-08-14 10:52:37 +0300

    initial commit

---
+++ Services/Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+
+}
---
+++ Services/Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mango.Services.ProductAPI.Models.Dtos;
+
+namespace Mango.Services.ProductAPI.Repository;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<ProductDto>> GetProducts();
+
+    Task<ProductDto> GetProductById(int productId);
+
+    Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+
+    Task<bool> DeleteProduct(int productId);
+
+}
